92dfd39bcd0574bbf7296f58dcbd5040c953f796
Madison Dickson <madison@builder.io>
2024-05-23 09:54:49 -0600

    remove title requirement

--- overrides/builder/blocks/einstein-products-scroller/index.jsx
+++ overrides/builder/blocks/einstein-products-scroller/index.jsx
@@ -3,11 +3,11 @@ import loadable from '@loadable/component'
 import {Skeleton} from '@chakra-ui/react'
 
 const fallback = <Skeleton height="75vh" width="100%" />
-const EinsteinProductsGrid = loadable(() => import('./einstein-products-grid'), {fallback})
+const EinsteinProductsScroller = loadable(() => import('./einstein-products-scroller'), {fallback})
 
-export const EinsteinProductsGridDefinition = {
-    component: EinsteinProductsGrid,
-    name: 'EinsteinProductsGrid',
+export const EinsteinProductsScrollerDefinition = {
+    component: EinsteinProductsScroller,
+    name: 'EinsteinProductsScroller',
     image: 'https://cdn.builder.io/api/v1/image/assets%2Fd1ed12c3338144da8dd6b63b35d14c30%2F671167ab7faa41d59624c88acf109360',
     defaults: {
         bindings: {
