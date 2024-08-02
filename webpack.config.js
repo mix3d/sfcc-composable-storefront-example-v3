743e3d760f2d3fdb6fa6d11fd3109b97bfff46fa
Madison Dickson <madison@builder.io>
2024-08-02 10:42:33 -0700

    Update Readme & add comments to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// Require is required for this file, hence the eslint-disable comment
 const config = require('@salesforce/pwa-kit-dev/configs/webpack/config')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 config.forEach((c) => {
+    // Add a new alias to the webpack config for easy imports in your project
+    // eg: import '~/app/constants' vs import '../../../app/constants'
     c.resolve = {
         ...c.resolve,
         alias: {
@@ -10,6 +14,8 @@ config.forEach((c) => {
             '~': path.resolve(__dirname, './overrides')
         }
     }
+    // add a CSS loader to handle CSS file imports directly
+    // Not required for Builder integration, but useful for most projects
     c.module.rules.push({
         test: /\.(sa|sc|c)ss$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
