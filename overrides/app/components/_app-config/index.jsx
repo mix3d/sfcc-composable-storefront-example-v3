aa40919b6a5d8690830fa266976f0c9ef6882555
Sami Jaber <jabersami@gmail.com>
2024-10-22 14:39:11 -0300

    try dynamic import in fetchHookGenerator

--- overrides/app/components/_app-config/index.jsx
+++ overrides/app/components/_app-config/index.jsx
@@ -1,4 +1,5 @@
 // Inject Custom Components Menu registration into the Composable App configuration.
+// import {withLegacyGetProps} from '@salesforce/pwa-kit-react-sdk/ssr/universal/components/with-legacy-get-props'
 import AppConfig from '@salesforce/retail-react-app/app/components/_app-config/index.jsx'
 import {register} from '@builder.io/sdk-react'
 
@@ -19,10 +20,30 @@ register('insertMenu', {
 // Note: Custom Components registration for Gen2 SDK is accomplished through passing an array of the Component Definitions to each <Content> component.
 // See /builder/blocks/index.js for the customComponents array, and
 // /app/pages/home/index.jsx for an example of passing the customComponents array to a <Content> component.
-console.log('DEBUG: AppConfig before initializeNodeRuntime import')
-import {initializeNodeRuntime} from '@builder.io/sdk-react/node/init'
-console.log('DEBUG: AppConfig after initializeNodeRuntime import')
-initializeNodeRuntime()
-console.log('DEBUG: AppConfig after initializeNodeRuntime call')
+// import ivm from 'isolated-vm'
+
+// function testIsolatedVM() {
+//     const foo = new ivm.Isolate().createContextSync().evalSync('1 + 1')
+
+//     console.log('testing isolated-vm execution: ', foo)
+// }
+
+// testIsolatedVM()
+
+// AppConfig.getProps = async () => {
+//     throw new Error('AppConfig.getProps not implemented')
+//     if (typeof window === 'undefined') {
+//         console.log('DEBUG: AppConfig.getProps')
+//         const {initializeNodeRuntime} = await import('@builder.io/sdk-react/node/init')
+//         initializeNodeRuntime()
+//     }
+
+//     return {}
+// }
+// console.log('DEBUG: AppConfig before initializeNodeRuntime import')
+// import {initializeNodeRuntime} from '@builder.io/sdk-react/node/init'
+// console.log('DEBUG: AppConfig after initializeNodeRuntime import')
+// initializeNodeRuntime()
+// console.log('DEBUG: AppConfig after initializeNodeRuntime call')
 
 export default AppConfig
