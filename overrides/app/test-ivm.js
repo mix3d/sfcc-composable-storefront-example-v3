8dc59940cc677d7b6b501e3347771758a81048bf
Sami Jaber <jabersami@gmail.com>
2024-10-22 18:54:08 -0300

    👹

--- overrides/app/test-ivm.js
+++
@@ -1,7 +0,0 @@
-import ivm from 'isolated-vm'
-
-export function testIsolatedVM() {
-    const foo = new ivm.Isolate().createContextSync().evalSync('1 + 1')
-
-    console.log('testing isolated-vm execution: ', foo)
-}
